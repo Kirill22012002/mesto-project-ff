a21f2c0c4d5811c44a4e9348d10041c58e65094b
Kirill Perepechkin <k.perepechkin@mightycall.ru>
2024-12-12 14:11:30 +0300

    fix changes

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const cardTemplate = document.querySelector("#card-template").content;
+const placesList = document.querySelector(".places__list");
+
+function createCard(card, removeCard) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = card.link;
+  cardElement.querySelector(".card__title").textContent = card.name;
+
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", removeCard);
+
+  return cardElement;
+}
+
+const removeCard = (evt) => {
+  const card = evt.target.parentElement;
+  card.remove();
+};
+
+initialCards.forEach((card) => placesList.append(createCard(card, removeCard)));
